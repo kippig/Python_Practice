f82bbccd116609d6fe03dc1a8431c559eb8ef519
kippig <cyprian.gascoigne@gmail.com>
2019-08-14 14:55:08 -0400

    4.11

---
+++ src/Cracking_the_coding_interview/Chapter_4_Trees_and_Graphs/4.11.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.insert(0, "../../Cracking_the_coding_interview/")
+from DataStructures.Graph import Tree, BST
+from random import randint
+
+
+def random_node(tree):
+    """
+    If we store the weights in the tree as a value, then we can swap the time/Space complexity
+    Time: O(n)
+    Space: O(log(n))
+    :param BST:
+    :return: random value, size of tree
+    """
+
+    options = [(tree.value, 1)] # array of values, weights
+    for c in tree.children():
+        if c is not None:
+            options.append(random_node(c))
+
+    weights = sum(map(lambda x: x[1], options))
+    lucky = randint(1, weights)
+    for o in options:
+        if lucky <= o[1]:
+            return o[0], weights
+        lucky -= o[1]
+
+
+bst = BST([x for x in range(6)])
+print(random_node(bst))
--- src/Cracking_the_coding_interview/DataStructures/Graph.py
+++ src/Cracking_the_coding_interview/DataStructures/Graph.py
@@ -41,11 +41,76 @@ class Tree:
 
 class BST(Tree):
 
-    def __init__(self, arr):
-        tree = self.create_minimal_bst(arr)
-        self.value = tree.value
-        self.lchild = tree.lchild
-        self.rchild = tree.rchild
+    def __init__(self, arr=None, value=None, lchild=None, rchild=None):
+        if arr is not None:
+            tree = self.create_minimal_bst(arr)
+            self.value = tree.value
+            self.lchild = tree.lchild
+            self.rchild = tree.rchild
+        else:
+            self.value = value
+            self.lchild = lchild
+            self.rchild = rchild
+
+    def insert(self, value):
+        if value > self.value:
+            if self.rchild is None:
+                self.rchild = BST(value=value)
+            else:
+                return self.rchild.insert(value)
+        elif value < self.value:
+            if self.lchild is None:
+                self.lchild = BST(value=value)
+            else:
+                return self.lchild.insert(value)
+        else:
+            print("Warning, value {} is already in tree".format(value))
+
+    def delete(self, value):
+        parent = self.find_parent(value)
+        if parent.lchild is not None and parent.lchild.value == value:
+            node, attr = parent.lchild, 'lchild'
+        elif parent.rchild is not None and parent.rchild.value == value:
+            node, attr = parent.rchild, 'rchild'
+        else:
+            print('Warning: value {} does not exist in Tree'.format(value))
+            return
+
+        if node.lchild is None:
+            if node.rchild is None:
+                setattr(parent, attr, None)
+            else:
+                setattr(parent, attr, node.rchild)
+        else:
+            new_value = node.lchild.find_max()
+            node.lchild.delete(new_value)
+            node.value = new_value
+
+    def find_max(self):
+        if self.rchild is None:
+            return self.value
+        return self.rchild.find_max()
+
+    def find(self, value):
+        if self.value == value:
+            return self
+        if (value < self.value or self.rchild is None) and self.lchild is not None:
+            return self.lchild.find(value)
+        elif (value > self.value or self.lchild is None) and self.rchild is not None:
+            return self.rchild.find(value)
+        else:
+            return None
+
+    def find_parent(self, value):
+        if self.lchild is not None and self.lchild.value == value:
+            return self
+        elif self.rchild is not None and self.rchild.value == value:
+            return self
+        elif self.lchild is not None and value < self.value:
+            return self.lchild.find_parent(value)
+        elif self.rchild is not None and value > self.value:
+            return self.rchild.find_parent(value)
+        return None
 
     def create_minimal_bst(self, arr, l_child_size=None):
         """
@@ -59,7 +124,7 @@ class BST(Tree):
         if len(arr) == 0:
             return None
         if len(arr) == 1:
-            return Tree(value=arr[0])
+            return BST(value=arr[0])
 
         if l_child_size is None:
             l_child_size, n = 0, 0
@@ -75,6 +140,7 @@ class BST(Tree):
         else:
             rchild = None
 
-        return Tree(value=arr[l_child_size],
+        return BST(value=arr[l_child_size],
                     lchild=self.create_minimal_bst(arr[:l_child_size], l_child_size=l_child_size),
                     rchild=rchild)
+
