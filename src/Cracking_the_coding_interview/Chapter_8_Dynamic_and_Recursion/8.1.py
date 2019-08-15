1fb73a8e023883eb46a46e32ff6a355d577fb7a7
kippig <cyprian.gascoigne@gmail.com>
2019-08-15 16:59:40 -0400

    chapter 6

---
+++ src/Cracking_the_coding_interview/Chapter_8_Dynamic_and_Recursion/8.1.py
@@ -0,0 +1,13 @@
+def triple_step(steps=10):
+    """f(n) = f(n-1) + f(n-2) + f(n-3) + 1"""
+    memo = [None] * steps
+    memo[0], memo[1], memo[2] = 1, 2, 4
+    if steps < 3:
+        return memo[steps]
+
+    for i in range(3, steps):
+        memo[i] = 1 + memo[i - 1] + memo[i - 2] + memo[i - 3]
+    return memo[-1]
+
+
+print(triple_step(5))
